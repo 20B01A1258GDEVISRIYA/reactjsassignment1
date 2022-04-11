0b0c971e73ac96116c7755bb24845f947486f6a3
Your Name <you@example.com>
2022-04-11 11:29:32 +0530

    assignment6

---
+++ assignment6/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Lists from './Lists';
+import Faker from 'faker';
+import './style.css';
+ReactDOM.render(
+    <Lists/>,
+    document.getElementById('root')
+);

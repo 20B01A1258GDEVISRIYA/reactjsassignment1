1be097765afa00f2fd4d2e024b3360a0a12278f0
Your Name <you@example.com>
2022-04-18 09:59:15 +0530

    digital clock

---
+++ digitalclock/src/Change.js
@@ -0,0 +1,9 @@
+// import React, {Component} from 'react';
+// class Change extends Component{
+//     render(){
+//         return (
+//         <h1>{this.props.title}</h1>
+//         )
+//     }
+// }
+// export default Change;
---
+++ digitalclock/src/Clock.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+class Clock extends Component{
+    constructor(props){
+        super(props);
+        this.state = {date: new Date()};
+    }
+componentDidMount(){
+        this.timer = setInterval(
+            () => this.setState({date: new Date()}),
+            1000
+        );
+    }
+componentWillUnmount(){
+        clearInterval(this.timer);
+    }
+render(){
+        return( 
+            <div className="clock">
+                <div className="timer">{this.state.date.toLocaleTimeString()}</div>
+            </div>
+        )
+    }
+}
+export default Clock;
